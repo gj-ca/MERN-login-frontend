d3a4c53f7362e69da15239947c7f7b546cf0c110
gj-ca <glen.johnson@coderacademy.edu.au>
2021-01-29 12:27:11 +1000

    credentials

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState, useEffect} from "react"
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(false)
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  })
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/users/me", {
+      credentials: 'include'
+    })
+    .then(data => data.json())
+    .then(json => {
+      // if (json.)
+      if (json.username) {
+        setLoggedInUser({json})
+      }
+    })
+  }, [])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch("http://localhost:5000/users/register", {
+      body: JSON.stringify(user),
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json"
+      },
+      credentials: 'include'
+    })
+    .then(data => data.json())
+    .then(json => {
+      setLoggedInUser(json)
+    })
+    // Send Data
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    fetch("http://localhost:5000/users/login", {
+      body: JSON.stringify(user),
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json"
+      },
+      credentials: 'include'
+    })
+    .then(data => data.json())
+    .then(json => {
+      setLoggedInUser(json)
+    })
+    // Send Data
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.name)
+    setUser({...user, [e.target.name]: e.target.value})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {!loggedInUser ? (
+      <>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username" >Username</label>
+          <input name="username" onChange={handleChange} value={user.username} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input name="password" onChange={handleChange} value={user.password} />
+        </div>
+        <button>Register!</button>
+      </form>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username" >Username</label>
+          <input name="username" onChange={handleChange} value={user.username} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input name="password" onChange={handleChange} value={user.password} />
+        </div>
+        <button>Login!</button>
+      </form>
+      </>
+    ) : (
+      <h2>You are logged in</h2>
+    )}
+    </>
+  )
 }
 
 export default App;
